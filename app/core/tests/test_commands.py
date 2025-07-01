bd47cc9b63a6cbb35fa920dbce5d542674f56694
Adepu Nithinvas <adepunithin987@gmail.com>
2025-07-01 21:56:43 +0530

    fixed lint issues in test_commands.py

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,5 +1,5 @@
 """
-test custom django management commands
+Test custom Django management commands.
 """
 
 from unittest.mock import patch
@@ -11,26 +11,26 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """Test custom django management commands"""
+    """Test custom Django management commands."""
 
     def test_wait_for_db_ready(self, patched_check):
-        """Test waiting for db when db is available"""
+        """Test waiting for DB when DB is available."""
         patched_check.return_value = True
 
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
 
-    
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """Test waiting for db when getting OperationalError"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
-                    [OperationalError]*3 + [True]
+        """Test waiting for DB when getting OperationalError."""
+        patched_check.side_effect = (
+            [Psycopg2Error] * 2 +
+            [OperationalError] * 3 +
+            [True]
+        )
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-
-        
